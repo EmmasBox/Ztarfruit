29e2f0d9828e88af01dcf833fc5e6a3558313742
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2024-11-19 14:45:21 -0500

    fixed corruption

--- ztarfruit.py
+++ ztarfruit.py
@@ -1,1 +1,38 @@
-{é¢£™†™¤‰£@†–™@ÙÁÃÆ{Ä…—…•„…•ƒ‰…¢z@×¨£ˆ–•@n~óKñòK§@•„@n~éÖÁä@ñKóK§{ä£‰“‰£¨@£–@¢–™£@£ˆ™–¤‡ˆ@–¤£—¤£@†™–”@ÉÙÙÄÂäðð@•„@ƒ™…£…@™…—–™£¢@‰•@¥™‰–¤¢@†–™”£¢†™–”@©–¤£‰“m—¨@‰”—–™£@„£¢…£¢†™–”@„£…£‰”…@‰”—–™£@„£…£‰”…‰”—–™£@–¢‰”—–™£@‘¢–•‰”—–™£@£–”““‰‚‰”—–™£@™…‰”—–™£@™‡—™¢…‰”—–™£@¢˜“‰£…ó—™¢…™@~@™‡—™¢…KÁ™‡¤”…•£×™¢…™M@@@@—™–‡~}é£™†™¤‰£@†–™@ÙÁÃÆ}k@@@@„…¢ƒ™‰—£‰–•~}ä£‰“‰£¨@£–@¢–™£@£ˆ™–¤‡ˆ@–¤£—¤£@†™–”@ÉÙÙÄÂäñðð@•„@ƒ™…£…@‘¢–•@–¤£—¤£}k]{Æ“‡¢@•„@™‡¤”…•£¢@£–@¢—…ƒ‰†¨@‰•@£ˆ…@ƒ–””•„@“‰•…—™¢…™K„„m™‡¤”…•£M}`‰}k@}``‰•—¤£}]—™¢…™K„„m™‡¤”…•£M}`™}k@}``™…¢…£}]—™¢…™K„„m™‡¤”…•£M}`–}k@}``–‚†¤¢ƒ£…}k@ƒ£‰–•~}¢£–™…m£™¤…}]•–¦@~@„£…£‰”…K•–¦M]@{@ƒ¤™™…•£@„£…@•„@£‰”…„£…m£‰”…@~@•–¦K¢£™†£‰”…M„`l”`l„`lè`£`lÈ`lÔ`lâ]“–‡m•”…@~@†©£™†™¤‰£mÀ„£…m£‰”…Ðm{“–„@¢…££‰•‡¢@†™–”@™©©K£–”“¦‰£ˆ@–—…•M©†™¤‰£K£–”“k@™‚]@¢@†z@@@@¢…££‰•‡¢@~@£–”““‰‚K“–„M†]„£‚¢…m¢…££‰•‡¢@~@¢…££‰•‡¢­„£‚¢…½{Ä£‚¢…@•”…„£‚¢…m•”…@~@„£‚¢…m¢…££‰•‡¢­•”…½ƒ–•@~@¢˜“‰£…óKƒ–••…ƒ£M†À„£‚¢…m•”…ÐK„‚]„‚mƒ¤™¢–™@~@ƒ–•Kƒ¤™¢–™M]
+#Zstarfruit for RACF
+#Dependencies: Python >=3.12.x and >=ZOAU 1.3.x
+#Utility to sort through output from IRRDBU00 and create reports in various formats
+
+from zoautil_py import datasets
+from datetime import datetime
+import os
+import json
+import tomllib
+import re
+import argparse
+import sqlite3
+
+parser = argparse.ArgumentParser(
+    prog='Ztarfruit for RACF',
+    description='Utility to sort through output from IRRDBU100 and create json output',
+)
+
+#Flags and arguments to specify in the command line
+parser.add_argument('-i', '--input')
+parser.add_argument('-r', '--reset')
+parser.add_argument('-o', '--obfuscate', action='store_true')
+
+now = datetime.now() # current date and time
+date_time = now.strftime("d-%m-%d-%Y-t-%H-%M-%S")
+log_name = f"ztarfruit_{date_time}_"
+
+#load settings from razz.toml
+with open("zfruit.toml", "rb") as f:
+    settings = tomllib.load(f)
+
+database_settings = settings["database"]
+
+#Database name
+database_name = database_settings["name"]
+con = sqlite3.connect(f"{database_name}.db")
+
+db_cursor = con.cursor()
