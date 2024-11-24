ca15f384d0f26225df6bcd2ab24b216efb7c1600
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2024-11-24 03:39:49 -0500

    Moved records to record.py, to keep things organized

--- ztarfruit.py
+++ ztarfruit.py
@@ -63,20 +63,3 @@ class Field:
 
     def get_range(self):
         return (self.start, self.end)
-    
-#0100
-group_basic_data_record_fields = [
-    Field(name="GPBD_NAME",data_type=DataType.Char,start=6,end=13),
-    Field(name="GPBD_SUPGRP_ID",data_type=DataType.Char,start=15,end=22),
-    Field(name="GPBD_CREATE_DATE",data_type=DataType.Char,start=24,end=33),
-    Field(name="GPBD_OWNER_ID",data_type=DataType.Char,start=35,end=42),
-    Field(name="GPBD_UACC",data_type=DataType.Char,start=44,end=51),
-    Field(name="GPBD_NOTERMUACC",data_type=DataType.Char,start=53,end=56),
-    Field(name="GPBD_INSTALL_DATA",data_type=DataType.Char,start=58,end=312),
-    Field(name="GPBD_MODEL",data_type=DataType.Char,start=314,end=357),
-    Field(name="GPBD_UNIVERSAL",data_type=DataType.Char,start=359,end=362)
-]
-
-records = [
-    Record(name="Group basic data record",identifier="0100",fields=group_basic_data_record_fields)
-]
